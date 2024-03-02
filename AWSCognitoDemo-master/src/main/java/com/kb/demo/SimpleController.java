cb690f6f103e79c50585e479594fe09b7b36fb95
admin <dudeobama246@gmail.com>
2024-03-02 16:31:15 +0530

    wokring on aws cognito security with springboot

--- AWSCognitoDemo-master/src/main/java/com/kb/demo/SimpleController.java
+++ AWSCognitoDemo-master/src/main/java/com/kb/demo/SimpleController.java
@@ -1,5 +1,6 @@
 package com.kb.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 
+	@Autowired
+	private CognitoService cogService;
+	
     @GetMapping(path = "/api/hello")
     public String getResp(){
         return  "Hey authenticated request!";
@@ -21,9 +25,19 @@ public class SimpleController {
     	return "Permit All is Working";
     }
     
-    @PostMapping(value = "/admin/greetMe", headers = "Accept=application/json") 
+    @PostMapping(value = "/admin/greetMe", consumes = MediaType.APPLICATION_JSON_VALUE) 
     public ResponseEntity<String> greetingAdmin(@RequestBody User user) {
     	
     	return ResponseEntity.ok("Hello, ADMIN! How do you do? "+user.getUsername()+" "+user.getEmail());
     }
+    
+  @PostMapping("/admin/register-user")
+  public String registerUser(@RequestBody User user) {
+  	try {
+  		cogService.addUserToUserPool(user.getUsername(), user.getEmail(), user.getPassword());
+  		return "Registratoin-Successful";
+  	}catch(Exception ex) {	
+  		return "Registration-Error!";
+  	}
+  }
 }
